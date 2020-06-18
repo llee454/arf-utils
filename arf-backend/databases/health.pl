5b1dbc937a3bd8a7bd5a3121a35a0dd33e3d403f
Larry Darryl Lee Jr <llee454@gmail.com>
2020-06-18 14:06:45 +0000

    Initialized the health database.

---
+++ arf-backend/databases/health.pl
@@ -0,0 +1,6 @@
+created(1592442378.0330372).
+assert(waistCircumference('958faa6c-b16c-11ea-86bd-0a29365a243a')).
+assert(bicepCircumference('9bd4626e-b16c-11ea-a74d-0a29365a243a')).
+assert(bloodPressureSystolic('a4fcf400-b16c-11ea-90c0-0a29365a243a')).
+assert(bloodPressureDiastolic('d2bc84dc-b16c-11ea-9c5e-0a29365a243a')).
+assert(pulse('db2fab30-b16c-11ea-ba9e-0a29365a243a')).
