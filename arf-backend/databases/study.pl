1ed2647c60ffcf14ef63239f74b89841d3282357
Larry Darryl Lee Jr <llee454@gmail.com>
2020-08-04 23:59:30 +0000

    Database backup

--- arf-backend/databases/study.pl
+++ arf-backend/databases/study.pl
@@ -1,3 +1,16 @@
 created(1592575150.2963784).
 assert(subject('0c9ebcba-b235-11ea-b786-0a29365a243a')).
 assert(subject('12b810e2-b235-11ea-b000-0a29365a243a')).
+assert(session('c7127db8-c052-11ea-b99e-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('e77917a0-c2ab-11ea-ba36-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('65fbee2a-c439-11ea-b055-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('8df1bea8-c517-11ea-8dcc-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('dd2cb0d6-c5e4-11ea-afa5-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('d001a5ce-c6a2-11ea-b593-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('22abe9c6-c778-11ea-8389-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('7bd1792e-ca86-11ea-9cf5-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('66a0fb64-cb9a-11ea-bfa1-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('ac729f38-cc32-11ea-96b6-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('0b99e66e-cd3d-11ea-b077-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('0d365b6c-d277-11ea-b361-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
+assert(session('fcb80a0a-d597-11ea-9405-0a29365a243a','0c9ebcba-b235-11ea-b786-0a29365a243a')).
